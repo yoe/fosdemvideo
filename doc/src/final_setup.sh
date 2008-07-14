17592f6374c13a02c4800c94cbab1e64a00865b5
Holger Levsen <holger@debian.org>
2008-07-14 22:56:48 +0000

    first draft of doc and automatic installation with d-i

---
+++ doc/src/final_setup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Copyright 2006-2008 Holger Levsen 
+# released under the GPLv=2
+#
+# this is a stripped down version :)
+
+DEBCONF_FRONTEND=noninteractive  DEBIAN_FRONTEND=noninteractive apt-get install vim fai-client subversion 
+
+echo 'FAI_CONFIG_SRC="svn//svn.debian.org/srv//fai-config-dir/trunk"' >> /etc/fai/fai.conf  # crude hack - last entry has precedence :)
+
+fai -N softupdate
+
+echo "cat /var/log/fai/current/error.log:"
+cat /var/log/fai/current/error.log
+
+echo
+echo "'fai -N softupdate' already ran."
+echo
+
