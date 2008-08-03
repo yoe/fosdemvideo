5f12df86d388b3a76d738f55716234437d8b3927
Holger Levsen <holger@debian.org>
2008-08-03 20:20:37 +0000

    make life easier

--- doc/src/final_setup.sh
+++ doc/src/final_setup.sh
@@ -5,6 +5,7 @@
 #
 # this is a stripped down version :)
 
+apt-get update
 DEBCONF_FRONTEND=noninteractive  DEBIAN_FRONTEND=noninteractive apt-get install vim fai-client subversion 
 
 echo 'FAI_CONFIG_SRC="svn//svn.debian.org/srv/debconf-video/fai-config"' >> /etc/fai/fai.conf  # crude hack - last entry has precedence :)
@@ -15,6 +16,6 @@ echo "cat /var/log/fai/current/error.log:"
 cat /var/log/fai/current/error.log
 
 echo
-echo "'fai -N softupdate' already ran."
+echo "'fai -N softupdate' done."
 echo
 
